70df1ac22cedb908f425b311c8b405850bc668e6
G3N1USpy <132838071+G3N1USpy@users.noreply.github.com>
2023-10-06 22:04:14 -0300

    Update codeofKAOS.py

--- codeofKAOS.py
+++ codeofKAOS.py
@@ -11,12 +11,11 @@ def limpar_tela():
 # Função para prever o próximo número secreto com base no histórico
 def prever_proximo_numero_secreto(historico):
     if len(historico) < 2:
-        return random.uniform(1.0, 10.0)  # Se não houver histórico, faça uma previsão aleatória entre 1.00 e 10.00
+        return random.uniform(1.0, 10.0)  # Se não houver histórico suficiente, faça uma previsão aleatória entre 1.00 e 10.00
 
-    # Exemplo de lógica simples: Use a média dos dois últimos números secretos
-    ultimo_numero_secreto = historico[-1]
-    penultimo_numero_secreto = historico[-2]
-    proximo_numero_secreto = (ultimo_numero_secreto + penultimo_numero_secreto) / 2.0
+    # Use a média de todos os números secretos anteriores como previsão
+    soma_numeros_secretos = sum(historico)
+    proximo_numero_secreto = soma_numeros_secretos / len(historico)
 
     return proximo_numero_secreto
 
@@ -30,14 +29,18 @@ COR_RESET = "\033[0m"
 # Loop para interagir com o usuário e atualizar o histórico
 while True:
     limpar_tela()  # Limpar a tela antes de cada print
+    print(" █▀▀█ █░░▒█ ▀█▀ █▀▀█ ▀▀█▀▀ █▀▀▀█ █▀▀█")
+    print(" █▄▄█ ▒█▒█░ ░█░ █▄▄█ ░▒█░░ █░░▒█ █▄▄▀")
+    print(" █░▒█ ░▀▄▀░ ▄█▄ █░▒█ ░▒█░░ █▄▄▄█ █░▒█")
+    print(COR_VERDE + "Feito por GENIUS" + COR_RESET)
+    print("")
     print(COR_VERDE + "Histórico de resultados:", historico_numeros_secretos)
     proxima_previsao = prever_proximo_numero_secreto(historico_numeros_secretos)
     print("Próxima previsão: {:.2f}".format(proxima_previsao) + COR_RESET)
     
     try:
         resultado_real = float(input(COR_VERDE + "Insira o resultado que caiu (formato 0.00): " + COR_RESET))
-        if resultado_real >= 1.0 and resultado_real <= 10.0:
-            historico_numeros_secretos.append(resultado_real)
+        historico_numeros_secretos.append(resultado_real)
             
     except ValueError:
         print(COR_VERMELHA + "Entrada inválida. Insira um número no formato 0.00 (ex: 3.55)." + COR_RESET)
